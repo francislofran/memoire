a501665b36e03c54885c85d2be2968984987923b
kukabi <163061140+francislofran@users.noreply.github.com>
2024-09-19 21:08:45 +0400

    Update googleLogin.jsx

--- src/app/components/googleLogin.jsx
+++ src/app/components/googleLogin.jsx
@@ -1,38 +1,12 @@
 
 
 import { useState } from 'react';
-import { auth, provider } from '../../firebaseConfig';
-import { signInWithPopup } from 'firebase/auth';
 
 const GoogleLogin = () => {
-  const [user, setUser] = useState(null);
-
-  const handleLogin = async () => {
-    try {
-      const result = await signInWithPopup(auth, provider);
-      setUser(result.user); // L'utilisateur est connecté, sauvegarde les infos
-      console.log(result.user);
-    } catch (error) {
-      console.error("Erreur lors de la connexion : ", error);
-    }
-  };
-
+ 
   return (
     <div>
-      {user ? (
-        <div>
-          <h1>Bienvenue, {user.displayName}</h1>
-          <img src={user.photoURL} alt="Photo de profil" />
-        </div>
-      ) : (
-
-        <button onClick={handleLogin}
-        className=""
-        >
-          Google
-     
-        </button>
-      )}
+    
     </div>
   );
 };
